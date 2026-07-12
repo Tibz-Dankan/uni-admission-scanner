69a2680f55980f8a68d8909967cd8d7de9d959a5
Tibz-Dankan <tibesigwadankan@gmail.com>
2026-07-12 21:57:44 +0300

    refactor: switch to using prisma dlx and pnpm start

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,11 +2,14 @@
 set -e
 
 echo "Running Prisma migrations..."
-pnpm exec prisma migrate deploy
+# pnpm exec prisma migrate deploy
+pnpm dlx prisma migrate deploy
 
 echo "Generating Prisma client..."
-pnpm exec prisma generate
+# pnpm exec prisma generate
+pnpm dlx prisma generate
 
 echo "Starting application..."
 # exec node dist/app.js
-pnpm dev
+# pnpm dev
+pnpm start
